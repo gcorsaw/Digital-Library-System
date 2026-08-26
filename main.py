236fc6a884861f666cfe8c9fdb3a2b2b67fe216a
gcorsaw <grace@corsaw.com>
2026-08-26 11:24:25 -0500

    The files that docker compose file, main.py have been modified. The database is a new file.

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
+from pickle import load
+
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# def config(filename='database.ini', section='postgresql'):
+#     parser = ConfigParser()
+#     parser.read(filename)
+
+#     database = {}
+#     if parser.has_section(section):
+#         params = parser.items(section)
+#         for param in params:
+#             database[param[0]] = param[1]
+#     else:
+#         raise Exception(f'Section {section} not found in the {filename} file')
+#     return database
+
+def connect():
+    """Connect to the PostgreSQL database server"""
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            dbname=os.environ.get("DB_NAME", "mydatabase"),
+            user=os.environ.get("DB_USER", "gcorsaw"),
+            password=os.environ.get("DB_PASSWORD", "DadR0cks"),
+            host=os.environ.get("DB_HOST", "localhost"),
+            port=os.environ.get("DB_PORT", 5440)
+        )
+        cursor = connection.cursor()
+        print("PostgreSQL database version:")
+        cursor.execute("SELECT version();")
+        version = cursor.fetchone()
+        print(version)
+
+        cursor.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+            print('Database connection closed.')
+
 def main():
     print("Hello from digital-library-system!")
-
+    connect()
 
 if __name__ == "__main__":
     main()
