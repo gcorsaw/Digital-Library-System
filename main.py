083ae5d7bc4700e63823ba68f45f36312a02805e
gcorsaw <grace@corsaw.com>
2026-08-26 17:53:19 -0500

    The python file has been modified and the reset.sql is to reset the database in Ubuntu.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from pickle import load
-
 import psycopg2
 import os
 from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
 
 load_dotenv()
 
@@ -29,7 +28,7 @@ def connect():
             user=os.environ.get("DB_USER", "gcorsaw"),
             password=os.environ.get("DB_PASSWORD", "DadR0cks"),
             host=os.environ.get("DB_HOST", "localhost"),
-            port=os.environ.get("DB_PORT", 5440)
+            port= int(os.environ.get("DB_PORT", 5440))
         )
         cursor = connection.cursor()
         print("PostgreSQL database version:")
@@ -45,9 +44,37 @@ def connect():
             connection.close()
             print('Database connection closed.')
 
+def get_book_database():
+    """Fetch and print all the rows from the book infor as a record set (list 
+    of dictionarys)"""
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            dbname=os.environ.get("DB_NAME", "mydatabase"),
+            user=os.environ.get("DB_USER", "gcorsaw"),
+            password=os.environ.get("DB_PASSWORD", "DadR0cks"),
+            host=os.environ.get("DB_HOST", "localhost"),
+            port= int(os.environ.get("DB_PORT", 5440))
+        )
+        cursor = connection.cursor(cursor_factory=RealDictCursor)
+        cursor.execute("SELECT * FROM book_info;")
+        records = cursor.fetchall()
+
+        print(f"Found {len(records)} books: \n")
+        for row in records:
+            print(row)
+
+        cursor.close()
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if connection is not None:
+            connection.close()
+
 def main():
     print("Hello from digital-library-system!")
     connect()
+    get_book_database()
 
 if __name__ == "__main__":
     main()
---
+++ reset.sql
@@ -0,0 +1,4 @@
+drop table if exists book_tracking;
+drop table if exists book_info;
+drop table if exists reader_info;
+drop table if exists author_info;
